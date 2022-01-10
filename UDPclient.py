f207530a5c126beafc7a0703b31452fd23e43d52
kanravee <kanravee.w@ku.th>
2022-01-10 18:29:21 +0700

    client

---
+++ UDPclient.py
@@ -0,0 +1,112 @@
+import json
+from socket import *
+
+def main():
+    serverName = 'localhost'
+    serverPort = 12000
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+    print('Provide the following information by integer')
+
+    while(1) :
+        try:
+            age = int(input('Input Age: '))
+            break
+        except ValueError:
+            print('Please enter an integer')
+
+    while(1) :
+        try:
+            sex = int(input('Input Sex (1=male, 0=female): '))
+            break
+        except ValueError:
+            print('Please enter an integer')
+
+    while(1) :
+        try:
+            chest_pain_type = int(input('Input chest pain type (1=typical angina, 2=atypical angina, 3=non-anginal pain, 4=asymptomatic): '))
+            break
+        except ValueError:
+            print('Please enter an integer')
+
+    while(1) :
+        try:
+            bp = int(input('Input resting blood pressure (in mmHg): '))
+            break
+        except ValueError:
+            print('Please enter an integer')
+
+    while(1) :
+        try:
+            cholesterol = int(input('Input serum cholestoral (in mg/dl): '))
+            break
+        except ValueError:
+            print('Please enter an integer')
+
+    while(1) :
+        try:
+            fbs = int(input('Fasting blood sugar > 120 mg/dl (1=true; 0=false): '))
+            break
+        except ValueError:
+            print('Please enter an integer')
+
+    while(1) :
+        try:
+            ekg_result = int(input('input resting electrocardiographic results (0=normal, 1=having ST-T wave abnormality, 2=showing probable left ventricular hypertrophy by Estes criteria): '))
+            break
+        except ValueError:
+            print('Please enter an integer')        
+
+    while(1) :
+        try:
+            max_hr = int(input('Input maximum heart rate achieved: '))
+            break
+        except ValueError:
+            print('Please enter an integer')
+
+    while(1) :
+        try:
+            exercise_angina = int(input('Exercise induced angina (1=yes; 0=no): '))
+            break
+        except ValueError:
+            print('Please enter an integer')   
+
+    while(1) :
+        try:
+            st_depression = float(input('Input ST depression induced by exercise relative to rest (decical value**): '))
+            break
+        except ValueError:
+            print('Please enter a decimal')   
+    
+    while(1) :
+        try:
+            slope_of_st = int(input('Input the slope of the peak exercise ST segment (1=upsloping, 2=flat, 3=downsloping): '))
+            break
+        except ValueError:
+            print('Please enter an integer')   
+    
+    while(1) :
+        try:
+            number_of_vessels_fluro = int(input('Input the number of major vessels colored by flourosopy (0-3): '))
+            break
+        except ValueError:
+            print('Please enter an integer')   
+    
+    while(1) :
+        try:
+            thallium = int(input('Input Thallium  (3=normal, 6=fixed defect, 7=reversable defect): '))
+            break
+        except ValueError:
+            print('Please enter an integer')   
+
+    information = json.dumps({'Age' : age, 'Sex' : sex, 'Chest pain type' : chest_pain_type, 'BP' : bp, 'Cholesterol' : cholesterol, 'FBS over 120' : fbs, 'EKG results' : ekg_result, 'Max HR' : max_hr, 'Exercise angina' : exercise_angina, 
+    'ST depression' : st_depression, 'Slope of ST' : slope_of_st, 'Number of vessels fluro' : number_of_vessels_fluro, 'Thallium' : thallium}).encode('utf-8')
+
+    clientSocket.sendto(information, (serverName, serverPort))
+    modifiedInformation, serverAddress = clientSocket.recvfrom(2048) 
+
+    print(modifiedInformation.decode())
+    clientSocket.close()
+
+if __name__ == "__main__":
+    main()
